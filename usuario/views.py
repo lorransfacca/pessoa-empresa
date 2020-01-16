d15643c9bcdf03f005afaca26295d53067a2caa4
Lorrans <lorransfac@gmail.com>
2020-01-16 15:17:22 -0300

    criar botões atualizar e deletar

--- usuario/views.py
+++ usuario/views.py
@@ -13,17 +13,21 @@ def mostrar_formulario_cadastro(request):
     pessoa.save()
     return render(request, 'login.html')
   return render(request, 'cadastrar_pessoa.html', args)
+  
+
 
 def mostrar_pessoas(request):
   pessoas = Pessoa.objects.all()
   return render(request, 'pessoas.html', {'dados': pessoas})
-
+  
+  
 def login(request):
   if request.method == 'POST':
     email_formulario = request.POST.get('email')
     pessoa_banco_dados = Pessoa.objects.filter(email=email_formulario).first()
     if pessoa_banco_dados is not None:
       return mostrar_pessoas(request)
+     
     return render(request, 'login.html', {'msg': 'Ops, não encontramos'})
 
   return render(request, 'login.html', {'msg': 'seja bem vindo'})
